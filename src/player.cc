28129f7ba8df8ae2e1959219a5b8110fca59bea4
seokholim <seokholim1@gmail.com>
2023-06-13 13:15:14 -0400

    set_name and constructor with player colour

--- src/player.cc
+++ src/player.cc
@@ -1,8 +1,12 @@
 #include "player.h"
 
-Player::Player() {}
+Player::Player(PlayerColour player_colour) : player_colour{player_colour} {}
 
 Player::Player(std::string name, PlayerColour player_colour) : name{name}, player_colour{player_colour} {}
+
+void Player::set_name(std::string name) {
+    this->name = name;
+}
 
 std::string Player::get_name() {
     return this->name;
--- src/player.h
+++ src/player.h
@@ -11,8 +11,9 @@ class Player {
     std::vector<Piece> pieces;
     
   public:  
-    Player();
+    Player(PlayerColour player_colour);
     Player(std::string name, PlayerColour player_colour);
+    void set_name(std::string name);
     std::string get_name();
     PlayerColour get_player_colour();
     
