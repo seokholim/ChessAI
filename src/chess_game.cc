112897cb2f64397aed71e17c0213648c9b9b8ea6
seokholim <seokholim1@gmail.com>
2023-06-13 13:13:09 -0400

    observer method finished with simple Rooks

--- src/chess_game.cc
+++ src/chess_game.cc
@@ -1,32 +1,52 @@
 #include <iostream>
 #include <string>
 #include "chess_game.h"
-#include "rook.h"
 
-
-ChessGame::ChessGame() : turn_number{0} {}
+ChessGame::ChessGame() : turn_number{0}, white_player{PlayerColour::White}, black_player{PlayerColour::Black}, board{} {}
 
 void ChessGame::initialize_players() {    
     std::string white_player_name, black_player_name;
     std::cout << "White player's name: " << std::endl;
     std::getline(std::cin, white_player_name);
-    this->white_player = Player(white_player_name, PlayerColour::White);
+    this->white_player.set_name(white_player_name);
 
     std::cout << "Black player's name: " << std::endl;
     std::getline(std::cin, black_player_name);
-    this->black_player = Player(black_player_name, PlayerColour::Black);
+    this->black_player.set_name(black_player_name);
 }
 
-void ChessGame::initialize_board() {
-    std::cout << "Setting up board" << std::endl;
-    this->board = Board();
+// set_piece_at(...) calls Board::set_piece_at(...) with observer method
+void ChessGame::set_piece_at(Piece* piece, Position new_pos) {
+    std::cout << "ChessGame::set_piece_at called" << std::endl;
+    // TODO: try catch
+    // try {
+    //     this->board.set_piece_at(piece, new_pos);
+    // } catch (const std::exception& e) {
+    //     std::cout << " a standard exception was caught, with message '"
+    //               << e.what() << "'\n";
+    // }
+    // std::cout << "after catch block" << std::endl;
+    std::vector <Piece*> pieces_on_board = board.get_pieces(); // use a pointer to vec?
+    for (auto it = pieces_on_board.begin(); it != pieces_on_board.end(); ++it) {
+        (*it)->attach(piece);
+        piece->attach(*it);
+    }
+    this->board.set_piece_at(piece, new_pos);
+    piece->notify();
+    piece->calculate_moves();
+}
 
+void ChessGame::initialize_pieces() {
     std::cout << "Creating pieces" << std::endl;
-    Rook* r1 = new Rook {&(this->white_player)};
-    this->board.set_piece_at(r1, {'a', 1});
+    Rook* r1 = new Rook {PlayerColour::White};
+    std::cout << "r1 made" << std::endl;
+    set_piece_at(r1, {'a', 1});
+    std::cout << "r1 set on board" << std::endl;
 
-    Rook* r2 = new Rook {&(this->black_player)};
-    this->board.set_piece_at(r2, {'h', 8});
+    Rook* r2 = new Rook {PlayerColour::Black};
+    std::cout << "r2 made" << std::endl;
+    set_piece_at(r2, {'b', 1});
+    std::cout << "r2 set on board" << std::endl;
 }
 
 void ChessGame::start() {
@@ -40,6 +60,9 @@ void ChessGame::start() {
         } else {
             std::cout << this->black_player.get_name() << "'s turn!" << std::endl;
         }
+        print_board();
+
+        // TODO: white can only move white pieces
         std::cin >> cmd;
 
         if (cmd == "move") {
@@ -50,14 +73,13 @@ void ChessGame::start() {
             int current_row = current_pos[1] - '0';
 
             // TODO: check current_pos is valid
-
             Position current_position {current_column, current_row};
-
             Piece* selected_piece = this->board.get_piece_at(current_position);
             if (selected_piece != nullptr) {
-                std::cout << selected_piece->print() << " selected" << std::endl;
+                selected_piece->print(); 
+                std::cout << " selected" << std::endl;
                 std::vector<Move*> possible_moves = selected_piece->get_moves();
-                std::cout << "You have the following moves:";
+                std::cout << "You have " << possible_moves.size() << " moves:";
                 for (int i = 0; i < possible_moves.size(); ++i) {
                     Position res = possible_moves[i]->get_move_to();
                     std::cout << res.column << res.row << ' ';
@@ -70,7 +92,6 @@ void ChessGame::start() {
             break; // testing turn 1
         }
     }
-
 }
 
 int ChessGame::get_turn() {
@@ -83,4 +104,8 @@ bool ChessGame::white_player_turn() {
 
 bool ChessGame::black_player_turn() {
     return (this->turn_number % 2) == 0; 
+}
+
+void ChessGame::print_board() {
+    this->board.print();
 }
