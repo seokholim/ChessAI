ddfca70e3098d2a764d4f2ff0fadf391215b2010
seokholim <seokholim1@gmail.com>
2023-06-13 13:15:52 -0400

    simple Rook for observer method

--- src/rook.cc
+++ src/rook.cc
@@ -1,27 +1,90 @@
 #include "rook.h"
+#include <iostream>
 
-Rook::Rook(Player* owner) : Piece{PieceType::Rook, owner} {}
+Rook::Rook(PlayerColour player_colour) : Piece{PieceType::Rook, player_colour} {}
 
-Rook::Rook(Player* owner, Position position, Board* board) : Piece{PieceType::Rook, owner, position, board} {}
+// Rook::Rook(Player* owner) : Piece{PieceType::Rook, owner} {}
 
-void Rook::update(Subject &by) {
-    this->moves.clear();
+// Rook::Rook(Player* owner, Position position, Board* board) : Piece{PieceType::Rook, owner, position, board} {}
 
-    // check Forward, Backward, Right, Left
+void Rook::calculate_moves() {
+    this->moves.clear(); // TODO: improve by not always clearing out moves
 
-    if (this->owner->get_player_colour() == PlayerColour::White) {
+    if (this->player_colour == PlayerColour::White) {
+        std::cout << "white player's Rook!" << std::endl;
         // Forward
         Position current_position = this->get_position();
+        std::cout << "This rook's current position is " << current_position.column << current_position.row << std::endl;
         current_position.row += 1;
-        while (this->board->empty_at(current_position)) {
-
+        std::cout << "This rook's current position was changed to " << current_position.column << current_position.row << std::endl;
+        if (this->board->empty_at(current_position)) {
+            std::cout << "Forward move made by White Rook!" << std::endl;
+            Move* new_move = new Move{this->position, current_position, this};
+            this->moves.push_back(new_move);
         }
 
         // Backward
         current_position = this->get_position();
         current_position.row -= 1;
-
-    } else {
+        if (this->board->empty_at(current_position)) {
+            std::cout << "Backward move made by White Rook!" << std::endl;
+            Move* new_move = new Move{this->position, current_position, this};
+            this->moves.push_back(new_move);
+        }
         
+        // Right
+        current_position = this->get_position();
+        current_position.column += 1;
+        if (this->board->empty_at(current_position)) {
+            Move* new_move = new Move{this->position, current_position, this};
+            this->moves.push_back(new_move);
+        }
+
+        // Left
+        current_position = this->get_position();
+        current_position.column -= 1;
+        if (this->board->empty_at(current_position)) {
+            Move* new_move = new Move{this->position, current_position, this};
+            this->moves.push_back(new_move);
+        }
+    } else {
+        std::cout << "Black player's Rook!" << std::endl;
+
+        // Forward
+        Position current_position = this->get_position();
+        current_position.row -= 1;
+        if (this->board->empty_at(current_position)) {
+            Move* new_move = new Move{this->position, current_position, this};
+            this->moves.push_back(new_move);
+        }
+
+        // Backward
+        current_position = this->get_position();
+        current_position.row += 1;
+        if (this->board->empty_at(current_position)) {
+            Move* new_move = new Move{this->position, current_position, this};
+            this->moves.push_back(new_move);
+        }
+        
+        // Right
+        current_position = this->get_position();
+        current_position.column -= 1;
+        if (this->board->empty_at(current_position)) {
+            Move* new_move = new Move{this->position, current_position, this};
+            this->moves.push_back(new_move);
+        }
+
+        // Left
+        current_position = this->get_position();
+        current_position.column += 1;
+        if (this->board->empty_at(current_position)) {
+            Move* new_move = new Move{this->position, current_position, this};
+            this->moves.push_back(new_move);
+        }
     }
+}
+
+void Rook::update() {
+    std::cout << "rook update called" << std::endl;
+    calculate_moves();
 }
--- src/rook.h
+++ src/rook.h
@@ -3,12 +3,16 @@
 #include "piece.h"
 #include "board.h"
 
+class Player;
+
 class Rook : public Piece {
 
 public:
+    Rook(PlayerColour player_colour);
     Rook(Player* owner); // position and board to be set
     Rook(Player* owner, Position position, Board* board);
-    void update(Subject &by) override;
+    void calculate_moves() override;
+    void update() override;
 };
 
 #endif
