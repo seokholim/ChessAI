fa6260a532979c90d34656a385a8117c910a2f55
seokholim <seokholim1@gmail.com>
2023-06-13 13:10:00 -0400

    constructor with player colour, and set_board and set_position

--- src/piece.cc
+++ src/piece.cc
@@ -1,6 +1,15 @@
 #include <iostream>
 #include "piece.h"
 
+Piece::Piece(PieceType piece_type, PlayerColour player_colour) {
+    if (valid_piece_type(piece_type) && (valid_player_colour(player_colour))) {
+        this->piece_type = piece_type;
+        this->player_colour = player_colour;
+    } else {
+        std::cout << "couldn't construct a piece" << std::endl;
+    }
+}
+
 Piece::Piece(PieceType piece_type, Player* owner) {
     if (valid_piece_type(piece_type) && (owner != nullptr)) {
         this->piece_type = piece_type;
@@ -25,16 +34,28 @@ PieceType Piece::get_piece_type() {
     return this->piece_type;
 }
 
+void Piece::set_position(Position new_pos) {
+    this->position = new_pos;
+}
+
 Position Piece::get_position() {
     return this->position;
+} 
+
+void Piece::set_board(Board* board) {
+    this->board = board;
 }
 
 std::vector<Move*> Piece::get_moves() {
     return this->moves;
 }
 
-char Piece::print() {
-    return convert_to_char(this->piece_type);
+bool Piece::in_range(Piece* other_piece) {
+
+}
+
+void Piece::print() {
+    std::cout<< convert_to_char(this->piece_type);
 }
 
 Piece::~Piece() {}
--- src/piece.h
+++ src/piece.h
@@ -4,7 +4,6 @@
 #include "subject.h"
 #include "info.h"
 #include "move.h"
-#include "player.h"
 
 class Board;
 
@@ -13,19 +12,25 @@ class Player;
 class Piece : public Subject, public Observer {
 protected:
     PieceType piece_type;
+    PlayerColour player_colour;
     Player* owner;
     Position position;
     Board* board;
     std::vector<Move*> moves;
 
 public:
+    Piece(PieceType piece_type, PlayerColour player_colour);
     Piece(PieceType piece_type, Player* owner);
     Piece(PieceType piece_type, Player* owner, Position position, Board* board);
     ~Piece();
     PieceType get_piece_type();
-    Position get_position();    
+    void set_position(Position new_pos);
+    Position get_position();
+    void set_board(Board* board);
+    virtual void calculate_moves() = 0;
     std::vector<Move*> get_moves();
-    char print();
+    bool in_range(Piece* other_piece);
+    void print();
 };
 
 #endif
