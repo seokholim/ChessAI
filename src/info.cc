f6b3c2732e47a90e5d477290935acde79a35bff5
seokholim <seokholim1@gmail.com>
2023-06-13 11:43:17 -0400

    rearranging and defining helpers

--- src/info.cc
+++ src/info.cc
@@ -1,12 +1,52 @@
 #include "info.h"
 #include <utility>
 
+bool valid_piece_type(PieceType piece_type) {
+    if (piece_type == PieceType::Pawn || piece_type == PieceType::Rook || piece_type == PieceType::Knight ||
+        piece_type == PieceType::Queen || piece_type == PieceType::King) {
+            return true;
+    }
+    return false;
+}
+
+char convert_to_char(PieceType piece_type) {
+    switch(piece_type) {
+        case PieceType::King:
+            return 'K';
+        case PieceType::Queen:
+            return 'Q';
+        case PieceType::Bishop:
+            return 'B';
+        case PieceType::Knight:
+            return 'N';
+        case PieceType::Rook:
+            return 'R';
+        case PieceType::Pawn:
+            return 'o';
+    }
+}
+
 Position::Position() {}
 
 Position::Position(char column, int row) : column{column}, row{row} {}
 
-char convert_to_char(PieceType piece_type) {
-    switch(piece_type) {
-        
+bool valid_position(Position position) {
+    if ((1 <= position.row && position.row <= 8) 
+    && ('a' <= position.column && position.column <= 'h')) return true;
+    return false;
+}
+
+bool valid_player_colour(PlayerColour player_colour) {
+    if (player_colour == PlayerColour::White || player_colour == PlayerColour::Black) return true;
+    return false;
+}
+
+bool valid_direction(Direction direction) {
+    if (direction == Direction::Forward || direction == Direction::Backward ||
+        direction == Direction::Right || direction == Direction::Left ||
+        direction == Direction::ForwardRight || direction == Direction::ForwardLeft ||
+        direction == Direction::BackwardRight || direction == Direction::BackwardLeft) {
+        return true;
     }
+    return false;
 }
--- src/info.h
+++ src/info.h
@@ -1,13 +1,19 @@
 #ifndef INFO_H
 #define INFO_H
 
-enum class PieceType {
-    Pawn, Rook, Knight, Queen, King
-};
-
 enum class PlayerColour {
     White, Black
 };
+
+bool valid_player_colour(PlayerColour player_colour);
+
+enum class PieceType {
+    Pawn, Rook, Knight, Bishop, Queen, King
+};
+
+bool valid_piece_type(PieceType piece_type);
+
+char convert_to_char(PieceType piece_type);
 
 struct Position {
     char column;
@@ -16,31 +22,13 @@ struct Position {
     Position(char col, int row);  
 };
 
-char convert_to_char(PieceType piece_type);
+bool valid_position(Position position);
 
 enum class Direction {
     Forward, Backward, Right, Left,
     ForwardRight, ForwardLeft, BackwardRight, BackwardLeft
 };
 
-bool valid_piece_type(PieceType piece_type) {
-    if (piece_type == PieceType::Pawn || piece_type == PieceType::Rook || piece_type == PieceType::Knight ||
-        piece_type == PieceType::Queen || piece_type == PieceType::King) {
-            return true;
-    }
-    return false;
-}
-
-bool valid_position(Position position) {}
-
-bool valid_direction(Direction direction) {
-    if (direction == Direction::Forward || direction == Direction::Backward ||
-        direction == Direction::Right || direction == Direction::Left ||
-        direction == Direction::ForwardRight || direction == Direction::ForwardLeft ||
-        direction == Direction::BackwardRight || direction == Direction::BackwardLeft) {
-        return true;
-    }
-    return false;
-}
+bool valid_direction(Direction direction);
 
 #endif
