abc92e9e66232d4a8aa169e98158ce35e87e9960
seokholim <seokholim1@gmail.com>
2023-06-16 17:34:55 -0400

    basic rook

--- src/rook.cc
+++ src/rook.cc
@@ -3,37 +3,29 @@
 
 Rook::Rook(PlayerColour player_colour) : Piece{PieceType::Rook, player_colour} {}
 
-// Rook::Rook(Player* owner) : Piece{PieceType::Rook, owner} {}
-
-// Rook::Rook(Player* owner, Position position, Board* board) : Piece{PieceType::Rook, owner, position, board} {}
-
 void Rook::calculate_moves() {
     this->moves.clear(); // TODO: improve by not always clearing out moves
 
     if (this->player_colour == PlayerColour::White) {
-        std::cout << "white player's Rook!" << std::endl;
         // Forward
         Position current_position = this->get_position();
-        std::cout << "This rook's current position is " << current_position.column << current_position.row << std::endl;
         current_position.row += 1;
-        std::cout << "This rook's current position was changed to " << current_position.column << current_position.row << std::endl;
-        if (this->board->empty_at(current_position)) {
-            std::cout << "Forward move made by White Rook!" << std::endl;
+        while (this->board->empty_at(current_position)) {
             Move* new_move = new Move{this->position, current_position, this};
             this->moves.push_back(new_move);
+            current_position.row += 1;
         }
 
         // Backward
-        current_position = this->get_position();
+        current_position = this->position;
         current_position.row -= 1;
         if (this->board->empty_at(current_position)) {
-            std::cout << "Backward move made by White Rook!" << std::endl;
             Move* new_move = new Move{this->position, current_position, this};
             this->moves.push_back(new_move);
         }
         
         // Right
-        current_position = this->get_position();
+        current_position = this->position;
         current_position.column += 1;
         if (this->board->empty_at(current_position)) {
             Move* new_move = new Move{this->position, current_position, this};
@@ -41,17 +33,15 @@ void Rook::calculate_moves() {
         }
 
         // Left
-        current_position = this->get_position();
+        current_position = this->position;
         current_position.column -= 1;
         if (this->board->empty_at(current_position)) {
             Move* new_move = new Move{this->position, current_position, this};
             this->moves.push_back(new_move);
         }
     } else {
-        std::cout << "Black player's Rook!" << std::endl;
-
         // Forward
-        Position current_position = this->get_position();
+        Position current_position = this->position;
         current_position.row -= 1;
         if (this->board->empty_at(current_position)) {
             Move* new_move = new Move{this->position, current_position, this};
@@ -59,7 +49,7 @@ void Rook::calculate_moves() {
         }
 
         // Backward
-        current_position = this->get_position();
+        current_position = this->position;
         current_position.row += 1;
         if (this->board->empty_at(current_position)) {
             Move* new_move = new Move{this->position, current_position, this};
@@ -67,7 +57,7 @@ void Rook::calculate_moves() {
         }
         
         // Right
-        current_position = this->get_position();
+        current_position = this->position;
         current_position.column -= 1;
         if (this->board->empty_at(current_position)) {
             Move* new_move = new Move{this->position, current_position, this};
@@ -75,7 +65,7 @@ void Rook::calculate_moves() {
         }
 
         // Left
-        current_position = this->get_position();
+        current_position = this->position;
         current_position.column += 1;
         if (this->board->empty_at(current_position)) {
             Move* new_move = new Move{this->position, current_position, this};
@@ -85,6 +75,5 @@ void Rook::calculate_moves() {
 }
 
 void Rook::update() {
-    std::cout << "rook update called" << std::endl;
     calculate_moves();
 }
