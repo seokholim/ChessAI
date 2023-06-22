25a37747e56d03b370a64a8563136fabeb310d64
seokholim <seokholim1@gmail.com>
2023-06-22 19:52:01 -0400

    move can be evaluated

--- src/move.cc
+++ src/move.cc
@@ -1,7 +1,28 @@
 #include "move.h"
 
-Move::Move(Position move_from, Position move_to, Piece* move_piece) : move_from{move_from}, move_to{move_to}, move_piece{move_piece} {}
+Move::Move(Position move_from, Position move_to, Piece* moving_piece, Piece* captured_piece) 
+    : move_from{move_from}, move_to{move_to}, moving_piece{moving_piece}, captured_piece{captured_piece}, value{0} {}
+
+Position Move::get_move_from() {
+    return move_from;
+}
 
 Position Move::get_move_to() {
-    return this->move_to;
+    return move_to;
+}
+
+void Move::set_captured(Piece* captured_piece) {
+    this->captured_piece = captured_piece;
+}
+
+Piece* Move::get_captured() {
+    return captured_piece;
+}
+
+void Move::set_value(int value) {
+    this->value = value;
+}
+
+int Move::get_value() {
+    return value;
 }
--- src/move.h
+++ src/move.h
@@ -7,10 +7,26 @@ class Piece;
 class Move {
     Position move_from;
     Position move_to;
-    Piece* move_piece;
+    Piece* moving_piece;
+    Piece* captured_piece;
+    int value;
   public:
-    Move(Position move_from, Position move_to, Piece* p);
+    Move(Position move_from, Position move_to, Piece* moving_piece, Piece* captured_piece);
+    Position get_move_from();
     Position get_move_to();
+    
+    void set_captured(Piece* captured_piece);
+    Piece* get_captured();
+
+    void set_value(int value);
+    int get_value();
+};
+
+class CompareMove {
+  public:
+    bool operator() (Move* m1, Move* m2) {
+        return m1->get_value() < m2->get_value();
+    }
 };
 
 #endif
