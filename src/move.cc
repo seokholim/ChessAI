29a829b576fc8ece070a178db9e50fdeb287fb6b
seokholim <seokholim1@gmail.com>
2023-07-02 15:19:50 -0400

    updating pointers to smart pointers

--- src/move.cc
+++ src/move.cc
@@ -1,28 +1,30 @@
 #include "move.h"
 
-Move::Move(Position move_from, Position move_to, Piece* moving_piece, Piece* captured_piece) 
-    : move_from{move_from}, move_to{move_to}, moving_piece{moving_piece}, captured_piece{captured_piece}, value{0} {}
+Move::Move(Position move_from, Position move_to, std::shared_ptr<Piece> moving_piece, std::shared_ptr<Piece> occupying_piece) 
+    : move_from_{move_from}, move_to_{move_to}, moving_piece_{moving_piece}, occupying_piece_{occupying_piece}, move_type_{MoveType::Unevaluated}, value_{} {}
 
-Position Move::get_move_from() {
-    return move_from;
+Move::~Move() {}
+
+Position Move::move_from() const {
+    return move_from_;
 }
 
-Position Move::get_move_to() {
-    return move_to;
+Position Move::move_to() const {
+    return move_to_;
 }
 
-void Move::set_captured(Piece* captured_piece) {
-    this->captured_piece = captured_piece;
+std::shared_ptr<Piece> Move::moving_piece() const {
+    return moving_piece_;
 }
 
-Piece* Move::get_captured() {
-    return captured_piece;
+std::shared_ptr<Piece> Move::occupying_piece() const {
+    return occupying_piece_;
 }
 
-void Move::set_value(int value) {
-    this->value = value;
+MoveType Move::move_type() const {
+    return move_type_;
 }
 
-int Move::get_value() {
-    return value;
+int Move::value() const {
+    return value_;
 }
--- src/move.h
+++ src/move.h
@@ -1,31 +1,37 @@
 #ifndef MOVE_H
 #define MOVE_H
+#include <iostream>
 #include "info.h"
 
 class Piece;
 
 class Move {
-    Position move_from;
-    Position move_to;
-    Piece* moving_piece;
-    Piece* captured_piece;
-    int value;
-  public:
-    Move(Position move_from, Position move_to, Piece* moving_piece, Piece* captured_piece);
-    Position get_move_from();
-    Position get_move_to();
-    
-    void set_captured(Piece* captured_piece);
-    Piece* get_captured();
+public:
+    Move(Position move_from, Position move_to, std::shared_ptr<Piece> moving_piece, std::shared_ptr<Piece> occupying_piece);
+    ~Move();
 
-    void set_value(int value);
-    int get_value();
+    Position move_from() const;
+    Position move_to() const;
+
+    std::shared_ptr<Piece> moving_piece() const;
+    std::shared_ptr<Piece> occupying_piece() const;
+
+    MoveType move_type() const;
+    
+    int value() const;
+private:
+    Position move_from_;
+    Position move_to_;
+    std::shared_ptr<Piece> moving_piece_;
+    std::shared_ptr<Piece> occupying_piece_;
+    MoveType move_type_;
+    int value_;
 };
 
 class CompareMove {
-  public:
-    bool operator() (Move* m1, Move* m2) {
-        return m1->get_value() < m2->get_value();
+public:
+    bool operator() (std::shared_ptr<Move> m1, std::shared_ptr<Move> m2) {
+        return m1->value() < m2->value();
     }
 };
 
