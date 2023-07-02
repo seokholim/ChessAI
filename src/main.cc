8fee3c8b9f01fc7d47ac7466aa2e52231339690f
seokholim <seokholim1@gmail.com>
2023-07-02 15:19:32 -0400

    use init

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,13 @@
 #include <iostream>
 #include "chess_game.h"
 
-using namespace std;
-
 int main () {
     
     ChessGame g;
 
-    g.initialize_players();
+    g.init();
 
-    g.initialize_pieces();
-
-    g.start();
+    g.run();
 
     return 0;
 }
