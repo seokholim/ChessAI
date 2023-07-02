da423501452cee7291f68ad169ae44d601ac726d
seokholim <seokholim1@gmail.com>
2023-07-02 15:20:18 -0400

    updating pointers to smart pointers

--- src/subject.cc
+++ src/subject.cc
@@ -1,10 +1,10 @@
 #include "subject.h"
 
-void Subject::attach(Observer* ob) {
+void Subject::attach(std::shared_ptr<Observer> ob) {
     this->observers.push_back(ob);
 }
 
-void Subject::detach(Observer* ob) {
+void Subject::detach(std::shared_ptr<Observer> ob) {
     for (auto it = observers.begin(); it != observers.end(); ++it) {
         if (*it == ob) {
             observers.erase(it);
--- src/subject.h
+++ src/subject.h
@@ -1,14 +1,15 @@
 #ifndef SUBJECT_H
 #define SUBJECT_H
-#include "observer.h"
 #include <vector>
+#include <memory>
+#include "observer.h"
 
-class Subject {
+class Subject { // TODO: position
   protected:
-    std::vector<Observer*> observers;
+    std::vector<std::shared_ptr<Observer>> observers;
   public:
-    void attach(Observer* ob);
-    void detach(Observer* ob);
+    void attach(std::shared_ptr<Observer> ob);
+    void detach(std::shared_ptr<Observer> ob);
     void notify();
 };
 
