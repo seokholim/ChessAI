c90625edd14e1e0df0481a5dc370fbd1486ec6d0
seokholim <seokholim1@gmail.com>
2023-07-07 22:30:45 -0400

    const values for capture moves; unbalanced_trade; new move_types

--- src/info.cc
+++ src/info.cc
@@ -1,5 +1,4 @@
 #include "info.h"
-#include <utility>
 
 bool valid_piece_type(PieceType piece_type) {
     if (piece_type == PieceType::Pawn || piece_type == PieceType::Rook || piece_type == PieceType::Knight ||
@@ -7,6 +6,25 @@ bool valid_piece_type(PieceType piece_type) {
             return true;
     }
     return false;
+}
+
+bool unbalanced_trade(PieceType trading, PieceType with) {
+    if (trading == PieceType::Pawn) {
+        return false;
+    } else if (trading == PieceType::Bishop || trading == PieceType::Knight || trading == PieceType::Rook) {
+        if (with != PieceType::Pawn) {
+            return false;
+        }
+        return true;
+    } else if (trading == PieceType::Queen) {
+        if (with == PieceType::Queen) {
+            return false;
+        }
+        return true;
+    } else if (trading == PieceType::King) { // logically impossible
+        return false;
+    }
+    return true;
 }
 
 std::string convert_to_char(PlayerColour player_colour, PieceType piece_type) {
@@ -52,10 +70,18 @@ Position::Position() {}
 
 Position::Position(char column, int row) : column{column}, row{row} {}
 
+bool Position::operator==(const Position& other_pos) const {
+    return (this->column == other_pos.column && this->row == other_pos.row);
+}
+
 bool valid_position(Position position) {
     if ((1 <= position.row && position.row <= 8) 
     && ('a' <= position.column && position.column <= 'h')) return true;
     return false;
+}
+
+bool increasing_column(Position pos1, Position pos2) {
+    return pos1.column < pos2.column;
 }
 
 bool valid_player_colour(PlayerColour player_colour) {
@@ -71,4 +97,38 @@ bool valid_direction(Direction direction) {
         return true;
     }
     return false;
+}
+
+bool valid_chess_move_type(MoveType move_type) {
+    if (move_type == MoveType::Capturing || move_type == MoveType::Evading || move_type == MoveType::Neutral
+    ||  move_type == MoveType::Castling || move_type == MoveType::EnPassant || move_type == MoveType::Upgrade
+    ||  move_type == MoveType::Dangerous || move_type == MoveType::CapturingForFree || move_type == MoveType::SafeNeutral) {
+        return true;
+    }
+    return false;
+}
+
+bool is_move_t1(MoveType move_type) {
+    if (move_type == MoveType::Castling) {
+        return true;
+    }
+    return false;
+}
+
+int safe_division(int num, int den) { // where den > 0
+    if (den <= 0) {
+        return 0;
+    }
+    int absolute_num = 1;
+    if (num < 0) {
+        absolute_num = -num;
+    } else {
+        absolute_num = num;
+    }
+    if (absolute_num < den) {
+        return 0;
+    } else if (num < 0) {
+        return -(absolute_num / den);
+    }
+    return num / den;
 }
--- src/info.h
+++ src/info.h
@@ -1,18 +1,21 @@
 #ifndef INFO_H
 #define INFO_H
 #include <string>
+#include <limits>
 
 enum class PlayerColour {
-    White, Black
+    Null, White, Black
 };
 
 bool valid_player_colour(PlayerColour player_colour);
 
 enum class PieceType {
-    Pawn, Rook, Knight, Bishop, Queen, King
+    Null, Pawn, Rook, Knight, Bishop, Queen, King
 };
 
 bool valid_piece_type(PieceType piece_type);
+
+bool unbalanced_trade(PieceType trading, PieceType with);
 
 std::string convert_to_char(PlayerColour player_colour, PieceType piece_type);
 
@@ -21,15 +24,72 @@ struct Position {
     int row;
     Position();
     Position(char col, int row);  
+    bool operator==(const Position& other_pos) const;	
 };
 
 bool valid_position(Position position);
 
+bool increasing_column(Position pos1, Position pos2);
+
 enum class Direction {
-    Forward, Backward, Right, Left,
+    Null, Forward, Backward, Right, Left,
     ForwardRight, ForwardLeft, BackwardRight, BackwardLeft
 };
 
 bool valid_direction(Direction direction);
+
+enum class MoveType {
+    Null, Unevaluated, Capturing, CapturingForFree, Defending, Dangerous, Evading, Neutral, SafeNeutral, Castling, EnPassant, Upgrade, KingUnsafe
+};
+
+bool valid_chess_move_type(MoveType move_type);
+
+bool is_move_t1(MoveType MoveType);
+
+int safe_division(int a, int b);
+
+// Capture Move base values
+
+const int pawn_capturing_pawn = 30;
+const int pawn_capturing_rook = 150;
+const int pawn_capturing_knight = 150;
+const int pawn_capturing_bishop = 150;
+const int pawn_capturing_queen = 300;
+const int pawn_capturing_king = 20;
+
+const int rook_capturing_pawn = 10;
+const int rook_capturing_rook = 50;
+const int rook_capturing_knight = 50;
+const int rook_capturing_bishop = 50;
+const int rook_capturing_queen = 300;
+const int rook_capturing_king = 20;
+
+const int knight_capturing_pawn = 10;
+const int knight_capturing_rook = 50;
+const int knight_capturing_knight = 50;
+const int knight_capturing_bishop = 50;
+const int knight_capturing_queen = 300;
+const int knight_capturing_king = 20;
+
+const int bishop_capturing_pawn = 10;
+const int bishop_capturing_rook = 50;
+const int bishop_capturing_knight = 50;
+const int bishop_capturing_bishop = 50;
+const int bishop_capturing_queen = 300;
+const int bishop_capturing_king = 20;
+
+const int queen_capturing_pawn = 10;
+const int queen_capturing_rook = 50;
+const int queen_capturing_knight = 50;
+const int queen_capturing_bishop = 50;
+const int queen_capturing_queen = 300;
+const int queen_capturing_king = 20;
+
+const int king_capturing_pawn = 5;
+const int king_capturing_rook = 5;
+const int king_capturing_knight = 5;
+const int king_capturing_bishop = 5;
+const int king_capturing_queen = 10;
+const int king_capturing_king = 5;
 
 #endif
