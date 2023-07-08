f57b093ba52a2189be4da3304c29d33fe1a47d52
seokholim <seokholim1@gmail.com>
2023-07-07 22:31:05 -0400

    move tier 1,2

--- src/piece.cc
+++ src/piece.cc
@@ -1,13 +1,16 @@
 #include "piece.h"
 
 Piece::Piece(PieceType type, PlayerColour colour) 
-    : type_{type}, colour_{colour}, starting_position_{}, position_{}, board_{}, candidate_positions_{}, moves_{}, move_history_{} {
+    : type_{type}, colour_{colour}, starting_position_{}, position_{}, board_{}, candidate_positions_{}, moves_t1_{}, moves_t2_{}, move_history_{} {
     if (!valid_piece_type(type) || !valid_player_colour(colour)) {
         std::cout << "Piece::Piece(...) invalid construction!" << std::endl;
     }
+    // std::cout << "Piece::Piece() " << std::endl; 
 }
 
-Piece::~Piece() {}
+Piece::~Piece() { 
+    // std::cout << "Piece::~Piece() " << std::endl; 
+}
 
 void Piece::starting_position(const Position& starting_position) {
     if (!valid_position(starting_position)) {
@@ -23,11 +26,15 @@ void Piece::position(const Position& position) {
     position_ = position;
 }
 
-void Piece::set_board(ChessBoard* board) {
+void Piece::set_board(std::shared_ptr<ChessBoard> board) {
     if (board == nullptr) {
         std::cout << "set_board received nullptr!" << std::endl;
     }
     board_ = board;
+}
+
+std::shared_ptr<ChessBoard> Piece::board() const {
+    return board_;
 }
 
 bool Piece::has_board() {
@@ -48,13 +55,20 @@ std::queue<Position> Piece::candidate_positions() {
 }
 
 void Piece::clear_moves() {
-    while (!moves_.empty()) {
-        moves_.pop();
+    while (!moves_t1_.empty()) {
+        moves_t1_.pop();
+    }
+    while (!moves_t2_.empty()) {
+        moves_t2_.pop();
     }
 }
 
-void Piece::add_move(std::shared_ptr<Move> new_move) {
-    moves_.push(new_move);
+void Piece::add_move_t1(std::shared_ptr<Move> new_move) {
+    moves_t1_.push(new_move);
+}
+
+void Piece::add_move_t2(std::shared_ptr<Move> new_move) {
+    moves_t2_.push(new_move);
 }
 
 PieceType Piece::type() const {
@@ -73,6 +87,10 @@ bool Piece::black() const {
     return colour_ == PlayerColour::Black;
 }
 
+Position Piece::starting_position() const {
+    return starting_position_;
+}
+
 Position Piece::position() const {
     return position_;
 } 
@@ -85,17 +103,28 @@ bool Piece::first_move() const {
     return move_history_.empty();
 }
 
-std::priority_queue<std::shared_ptr<Move>, std::vector<std::shared_ptr<Move>>, CompareMove> Piece::moves() const {
-    return moves_;
+std::priority_queue<std::shared_ptr<Move>, std::vector<std::shared_ptr<Move>>, CompareMove> Piece::moves_t1() const {
+    return moves_t1_;
+}
+
+std::priority_queue<std::shared_ptr<Move>, std::vector<std::shared_ptr<Move>>, CompareMove> Piece::moves_t2() const {
+    return moves_t2_;
 }
 
 std::shared_ptr<Move> Piece::best_move() const {
-    if (moves_.empty()) {
-        return nullptr;
+    if (moves_t1_.empty()) {
+        if (moves_t2_.empty()) {
+            return nullptr;
+        }
+        return moves_t2_.top();
     }
-    return moves_.top();
+    return moves_t1_.top();
 }
 
 void Piece::print() const {
     std::cout<< convert_to_char(colour_, type_);
+}
+
+void Piece::add_to_history(std::shared_ptr<Move> move) {
+    move_history_.push_back(move);
 }
--- src/piece.h
+++ src/piece.h
@@ -16,7 +16,8 @@ public:
     void starting_position(const Position& starting_position);
     void position(const Position& position);
 
-    void set_board(ChessBoard* board);
+    void set_board(std::shared_ptr<ChessBoard> board);
+    std::shared_ptr<ChessBoard> board() const;
     bool has_board();
 
     virtual void generate_candidate_positions() = 0;
@@ -24,7 +25,8 @@ public:
     std::queue<Position> candidate_positions();
 
     void clear_moves();
-    void add_move(std::shared_ptr<Move> new_move);
+    void add_move_t1(std::shared_ptr<Move> new_move);
+    void add_move_t2(std::shared_ptr<Move> new_move);
 
     // ChessBoard, ChessPiece can also call:
     PieceType type() const;
@@ -33,26 +35,33 @@ public:
     bool white() const;
     bool black() const;
 
+    Position starting_position() const;
     Position position() const;
     bool in_range(const std::shared_ptr<Piece> other_piece) const; // TODO
 
     bool first_move() const;
 
-    std::priority_queue<std::shared_ptr<Move>, std::vector<std::shared_ptr<Move>>, CompareMove> moves() const;
+    std::priority_queue<std::shared_ptr<Move>, std::vector<std::shared_ptr<Move>>, CompareMove> moves_t1() const;
+    std::priority_queue<std::shared_ptr<Move>, std::vector<std::shared_ptr<Move>>, CompareMove> moves_t2() const;
     std::shared_ptr<Move> best_move() const;
+
+    void add_to_history(std::shared_ptr<Move> move);
 
     void print() const;
 protected:
     const PieceType type_;
     const PlayerColour colour_;
-    Position starting_position_; // TODO: check if const works
+    Position starting_position_;
     Position position_;
-    ChessBoard* board_;
+    std::shared_ptr<ChessBoard> board_;
 
     std::queue<Position> candidate_positions_;
-    std::priority_queue<std::shared_ptr<Move>, std::vector<std::shared_ptr<Move>>, CompareMove> moves_; // set by ChessEngine
+    std::priority_queue<std::shared_ptr<Move>, std::vector<std::shared_ptr<Move>>, CompareMove> moves_t1_;
+    std::priority_queue<std::shared_ptr<Move>, std::vector<std::shared_ptr<Move>>, CompareMove> moves_t2_;
 
     std::vector<std::shared_ptr<Move>> move_history_;
+
+    friend class Chess; // TESTING
 };
 
 #endif
