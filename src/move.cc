1878c10f41a5d2d3fa4eb7b19f1d7aa0a136cdd4
seokholim <seokholim1@gmail.com>
2023-07-07 22:27:08 -0400

    naming

--- src/move.cc
+++ src/move.cc
@@ -1,7 +1,7 @@
 #include "move.h"
 
-Move::Move(Position move_from, Position move_to, std::shared_ptr<Piece> moving_piece, std::shared_ptr<Piece> occupying_piece) 
-    : move_from_{move_from}, move_to_{move_to}, moving_piece_{moving_piece}, occupying_piece_{occupying_piece}, move_type_{MoveType::Unevaluated}, value_{} {}
+Move::Move(Position move_from, Position move_to, std::shared_ptr<ChessPiece> moving_piece, std::shared_ptr<ChessPiece> occupying_piece) 
+    : move_from_{move_from}, move_to_{move_to}, moving_piece_{moving_piece}, occupying_piece_{occupying_piece}, move_type_{MoveType::Unevaluated}, value_{0} {}
 
 Move::~Move() {}
 
@@ -13,16 +13,28 @@ Position Move::move_to() const {
     return move_to_;
 }
 
-std::shared_ptr<Piece> Move::moving_piece() const {
+std::shared_ptr<ChessPiece> Move::moving_piece() const {
     return moving_piece_;
 }
 
-std::shared_ptr<Piece> Move::occupying_piece() const {
+std::shared_ptr<ChessPiece> Move::occupying_piece() const {
     return occupying_piece_;
+}
+
+bool Move::occupied() const {
+    return occupying_piece_ != nullptr;
+}
+
+void Move::move_type(MoveType move_type) {
+    move_type_ = move_type;
 }
 
 MoveType Move::move_type() const {
     return move_type_;
+}
+
+void Move::value(int new_value) {
+    value_ = new_value;
 }
 
 int Move::value() const {
--- src/move.h
+++ src/move.h
@@ -3,27 +3,30 @@
 #include <iostream>
 #include "info.h"
 
-class Piece;
+class ChessPiece;
 
 class Move {
 public:
-    Move(Position move_from, Position move_to, std::shared_ptr<Piece> moving_piece, std::shared_ptr<Piece> occupying_piece);
+    Move(Position move_from, Position move_to, std::shared_ptr<ChessPiece> moving_piece, std::shared_ptr<ChessPiece> occupying_piece);
     ~Move();
 
     Position move_from() const;
     Position move_to() const;
 
-    std::shared_ptr<Piece> moving_piece() const;
-    std::shared_ptr<Piece> occupying_piece() const;
+    std::shared_ptr<ChessPiece> moving_piece() const;
+    std::shared_ptr<ChessPiece> occupying_piece() const;
+    bool occupied() const;
 
+    void move_type(MoveType move_type);
     MoveType move_type() const;
     
+    void value(int new_value);
     int value() const;
 private:
     Position move_from_;
     Position move_to_;
-    std::shared_ptr<Piece> moving_piece_;
-    std::shared_ptr<Piece> occupying_piece_;
+    std::shared_ptr<ChessPiece> moving_piece_;
+    std::shared_ptr<ChessPiece> occupying_piece_;
     MoveType move_type_;
     int value_;
 };
