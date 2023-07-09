accc788c8f1eb9ed6bc1b42a14b2221a7406005d
seokholim <seokholim1@gmail.com>
2023-07-09 17:19:51 -0400

    static member functions, and no fields

--- src/engine/engine.cc
+++ src/engine/engine.cc
@@ -4,8 +4,6 @@ bool engine_evaluation_debug = false;
 
 Engine::Engine() {}
 
-Engine::Engine(Board* board, std::vector<std::shared_ptr<Piece>> pieces) : board_{board}, pieces_{pieces} {}
-
 Engine::~Engine() {}
 
 void Engine::run_scenario(std::shared_ptr<Move>& move, int level) {
@@ -21,7 +19,7 @@ void Engine::run_scenario(std::shared_ptr<Move>& move, int level) {
     s.generate_next_scenarios();
     s.evaluate();
     if (engine_evaluation_debug) {
-        std::cout << "-- Parent Scenario for a move from " << move->move_from().column << move->move_from().row << " to " << move->move_to().column << move->move_to().row << " by ";
+        std::cout << "-- Scenario for a move from " << move->move_from().column << move->move_from().row << " to " << move->move_to().column << move->move_to().row << " by ";
         move->moving_piece()->print();
         std::cout << " has value: " << s.value() << std::endl;
     }
@@ -40,7 +38,7 @@ void Engine::evaluate_move(std::shared_ptr<Move>& move, int level) {
     } // TODO
 }
 
-void Engine::generate_moves_for(std::shared_ptr<ChessPiece>& piece, bool use_engine) {
+void Engine::generate_moves_for_piece(std::shared_ptr<ChessPiece>& piece, bool use_engine) {
     if (piece == nullptr || piece->data_ == nullptr) {
         std::cout<< "Engine::evaluate_moves_for(...); piece is nullptr" << std::endl;
         return;
@@ -52,29 +50,64 @@ void Engine::generate_moves_for(std::shared_ptr<ChessPiece>& piece, bool use_eng
     std::shared_ptr<Piece> piece_data = piece->data_;
     piece_data->clear_moves();
     std::queue<Position> candidate_positions = piece_data->candidate_positions();
+    std::vector<std::shared_ptr<Move>> candidate_moves;
+    std::shared_ptr<Move> candidate_move;
     while (!candidate_positions.empty()) {
         Position candidate_position = candidate_positions.front();
-        std::shared_ptr<Move> candidate_move;
-        std::shared_ptr<ChessPiece> occupying_piece = board_->get_piece_on(candidate_position);
+        std::shared_ptr<ChessPiece> occupying_piece = piece_data->board()->get_piece_on(candidate_position);
         if (occupying_piece != nullptr) {
-            candidate_move = std::make_shared<Move>(piece->position(), candidate_position, piece, board_->get_piece_on(candidate_position));
+            candidate_move = std::make_shared<Move>(piece->position(), candidate_position, piece, piece_data->board()->get_piece_on(candidate_position));
         } else {
             candidate_move = std::make_shared<Move>(piece->position(), candidate_position, piece, nullptr);
         }
-        evaluate_move(candidate_move, engine_level);
-        if (valid_chess_move_type(candidate_move->move_type())) {
-            if (is_move_t1(candidate_move->move_type())) {
-                piece_data->add_move_t1(candidate_move);
+        candidate_moves.push_back(candidate_move);
+        candidate_positions.pop();
+    }
+
+    // single thread
+    // for (auto& move : candidate_moves) {
+    //     evaluate_move(move, engine_level);
+    //     if (valid_chess_move_type(move->move_type())) {
+    //         if (is_move_t1(move->move_type())) {
+    //             piece_data->add_move_t1(move);
+    //         } else {
+    //             piece_data->add_move_t2(move);
+    //         }
+    //     }
+    // }
+
+    // multithreading
+    std::thread threads[candidate_moves.size()];
+    for (int i = 0; i < candidate_moves.size(); ++i) {
+        threads[i] = std::thread(&Engine::evaluate_move, std::ref(candidate_moves[i]), engine_level);
+    }
+    for (std::thread& t : threads) {
+        t.join();
+    }
+    for (auto& move : candidate_moves) {
+        if (valid_chess_move_type(move->move_type())) {
+            if (is_move_t1(move->move_type())) {
+                piece_data->add_move_t1(move);
             } else {
-                piece_data->add_move_t2(candidate_move);
+                piece_data->add_move_t2(move);
             }
         }
-        candidate_positions.pop();
     }
 }
 
-void Engine::generate_moves_for(std::vector<std::shared_ptr<ChessPiece>> pieces, bool use_engine) {
-    for (auto& piece : pieces) {
-        generate_moves_for(piece, use_engine);
+void Engine::generate_moves_for_pieces(std::vector<std::shared_ptr<ChessPiece>> pieces, bool use_engine) {
+    // single thread
+    // for (auto& piece : pieces) {
+    //     generate_moves_for_piece(piece, use_engine);
+    // }
+
+    // multithreading
+    std::thread threads[pieces.size()];
+    for (int i = 0; i < pieces.size(); ++i) {
+        threads[i] = std::thread(&Engine::generate_moves_for_piece, std::ref(pieces[i]), use_engine);
+    }
+
+    for (std::thread& t : threads) {
+        t.join();
     }
 }
--- src/engine/engine.h
+++ src/engine/engine.h
@@ -1,24 +1,24 @@
 #ifndef ENGINE_H
 #define ENGINE_H
+#include <thread>
 #include "scenario.h"
 #include "../board/board.h"
 #include "../pieces/chess_piece.h"
 #include "move.h"
 
+
 class Engine {
 public:
     Engine();
-    Engine(Board* board, std::vector<std::shared_ptr<Piece>> pieces);
     ~Engine();
 
-    void run_scenario(std::shared_ptr<Move>& move, int level);
-    void evaluate_move(std::shared_ptr<Move>& move, int level);
+    static void run_scenario(std::shared_ptr<Move>& move, int level);
+    static void evaluate_move(std::shared_ptr<Move>& move, int level);
     
-    void generate_moves_for(std::shared_ptr<ChessPiece>& piece, bool use_engine);
-    void generate_moves_for(std::vector<std::shared_ptr<ChessPiece>> pieces, bool use_engine);
+    static void generate_moves_for_piece(std::shared_ptr<ChessPiece>& piece, bool use_engine);
+    void generate_moves_for_pieces(std::vector<std::shared_ptr<ChessPiece>> pieces, bool use_engine);
 private:
-    Board* board_;
-    std::vector<std::shared_ptr<Piece>> pieces_;
+
 };
 
 #endif
